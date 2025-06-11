b4637297137f7316c82a35b85790ec4d70bc6f92
sparrow7559 <virajgupta2604@gmail.com>
2025-06-12 00:53:56 +0530

    Priority Queue

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -2,7 +2,10 @@
 import java.util.Queue;
 public class PriorityQueue{
     public static void main(String[] args) {
+        // prioity queue stores in heap in which the lowest goes in first
         Queue<Integer> pq = new java.util.PriorityQueue<>();
+        // Queue<Integer> pq = new java.util.PriorityQueue<>(Comparator.reverseOrder());
+        //Comparator.reverseOrder() can be used to reverse the prioity heap
         
         pq.offer(40);
         pq.offer(12);
@@ -13,5 +16,7 @@ public class PriorityQueue{
 
         pq.poll();
         System.out.println(pq);
+
+        System.out.println(pq.peek());
     }
 }
